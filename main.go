30d7ff7f68c65aeacaa3b24c4da555364d376087
AntanasMaziliauskas <maziliauskasantanas@gmail.com>
2019-01-24 14:34:01 +0200

    Test2

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
 type ExRates struct { // comment
@@ -13,12 +16,34 @@ type ExRates struct { // comment
 }
 
 func main() {
-
-	content, err := ioutil.ReadFile("rates.json")
-	if err != nil {
-		fmt.Println(err.Error())
+	//reader := bufio.newReader(os.Stdin)
+	//var opt string
+	var content []byte
+	fmt.Println("Press 'f' if you want to use File or press something else if you want to use URL...")
+	var opt string
+	fmt.Scanln(&opt)
+	if opt == "f" {
+		DocName := "rates.json"
+		content := FromDocument(DocName)
+	} else {
+		URLName := "https://api.exchangeratesapi.io/latest?base=USD"
+		content := FromURL(URLName)
 	}
-
+	//fmt.Println(first)
+	//opt, _ := reader.readString("\n")
+	/*switch opt {
+	case "F":
+		DocName := "rates.json"
+		content := FromDocument(DocName)
+	case "U":
+		URLName := "https://api.exchangeratesapi.io/latest?base=USD"
+		content := FromURL(URLName)
+	default:
+		fmt.Println("Error: Wrong button pressed.")
+		break
+	}*/
+	//DocName := "rates.json"
+	//content := FromDocument(DocName)
 	exrates := ExRates{}
 	// json.Unmarshal(content, &friends)
 	err2 := json.Unmarshal(content, &exrates)
@@ -37,4 +62,31 @@ func main() {
 		fmt.Printf("Currency: %s Value: \t%.2f\n", k, v)
 	}
 
+}
+
+func FromDocument(DocName string) []byte {
+	content, err := ioutil.ReadFile(DocName)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return content
+}
+
+func FromURL(URLName string) []byte {
+	spaceClient := http.Client{
+		Timeout: time.Second * 10, // 2 secs
+	}
+	req, err := http.NewRequest(http.MethodGet, URLName, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	return body
 }
